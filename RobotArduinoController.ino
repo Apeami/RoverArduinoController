a04870d974ad3fe4ae92115657d04f4d70c7936b
Apeami <>
2022-03-01 21:48:01 +0000

    Added New code

--- RobotArduinoController.ino
+++ RobotArduinoController.ino
@@ -3,44 +3,24 @@
 //Pin 10 - OCR1B - Rover Right Side
 //Pin A0-5 -  used for the inputs from the controller
 
-#define FREQ 16000000
-void pwmToPin(int pin, int dutyCycle, int frequency){
-//Valid values of "pin" are 9 and 10, all other values will be ignored
-//Duty cycle is a value from 1 to 1000 measured in percent
-//Frequency is a value in Hz
-
-
-//Step 1: Calculate values
-
-//Step 2: Set Waveform Generation Mode Bit Description
-//Mode 15: Fast PWM with TOP as ICR1. WGM13-10 = 1110
-
-//Step 3: Set Clock Select Bit Description
-//CS12-10 = based on calculated value
-//Clk/1 -- 001
-
-//Setp 4: Set Input Capture Register
-//ICR1H and ICR1L = based on calculated value
-//12422 = 0b 0011 0000 1000 0110
-
-//Step 5: Set Output Compare Register
-//OCR1AH, OCR1AL or OCR1BH, OCR1BL = based on dutyCycle from calculation
-
-//Step 6: Set Compare Output Mode, Fast PWM
-//Should be COM1A1/COM1B1=0 , COM1A0/COM1B0=1
- 
-}
-
 int getControllerValue(int pin){
   //returns value between 0 and 255
 
   //Constants that come from the control stick
-  const int minInput = 10;
-  const int maxInput = 50;
+  const int minInput = 0;
+  const int maxInput = 5000;
 
   //Read the analog value at the pin using the built in analog read function
-  int val = analogRead(pin);
-  Serial.println(val);
+  int val=0;
+  const int times = 50;
+
+  
+  while(analogRead(pin)>10);
+  long time1=millis();
+  while(analogRead(pin)<10);
+  while(analogRead(pin)>10);
+  Serial.println(millis()-time1);
+  Serial.println(millis());
 
   //Checks to see if the analog reading is within the correct range that is 
   //expected to come from the control stick
@@ -52,15 +32,62 @@ int getControllerValue(int pin){
   return ((val-minInput)*(255/(maxInput-minInput)));
 }
 
+void setupControllerReader(){
+  pinMode(2, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(2), readerHigh2, RISING);
+}
+
+int value2=0;
+long timeLast2=0;
+void readerHigh2(){
+   value2= millis()-timeLast2;
+   timeLast2 = timeLast2+value2;
+   //Serial.println("H");
+}
+
+int getControllerValue2(int pin){
+  if (pin==2){
+    return value2;
+  }
+}
+
+
+void setupMotorDriver(){
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(WGM11) ;
+  TCCR1B = _BV(WGM12)| _BV(WGM13) | _BV(CS12);
+  ICR1 = 1260;
+  OCR1A = 94.5;
+  OCR1B = 94.5;
+}
+
+void controlMotor(int side, int motorSpeed){
+  const int minValue=63;
+  const int maxValue=126;
+  const int range=maxValue-minValue;
+  
+  motorSpeed+=100;
+  
+  if (side==0){
+    OCR1A = ((motorSpeed/200)*range)+minValue;
+  }if (side==1){
+    OCR1B = ((motorSpeed/200)*range)+minValue;
+  }
+}
+
 
 void setup() {
   Serial.begin(9600);
+  setupMotorDriver();
+  setupControllerReader();
 
 }
 
 void loop() {
   //Desired frequency = 50Hz
   
-  //Serial.println("Message");
-  Serial.println(getControllerValue(A0),DEC);
+  Serial.println("Message");
+  Serial.println(getControllerValue2(2),DEC);
+  //controlMotor(0, -100);
 }
